a557f1c71d1bd880db7657bf0251011168afb3df
kayvee83 <kayvee.tmv@gmail.com>
2017-06-06 22:02:51 -0400

    Method for stats addded

--- src/main/java/com/kayvee/helloworld/controller/GreetingController.java
+++ src/main/java/com/kayvee/helloworld/controller/GreetingController.java
@@ -1,5 +1,12 @@
 package com.kayvee.helloworld.controller;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.lang.management.RuntimeMXBean;
+import java.util.StringJoiner;
+
+import javax.management.MBeanServer;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +18,7 @@ public class GreetingController {
 	@RequestMapping("/greeting")
 	@ResponseBody
 	public String greeting(String name) {
+		
 		return "Hello "+name;
 		
 	}
@@ -20,5 +28,22 @@ public class GreetingController {
 	public String hello() {
 		return "Hello User";
 	}
+	
+	@RequestMapping("/stats")
+	@ResponseBody
+	public String stats(){		RuntimeMXBean runtimeBean = ManagementFactory.getRuntimeMXBean();
+		
+		StringJoiner joiner = new StringJoiner("Statistics \n");
+		joiner.add("VM name ").add(runtimeBean.getVmName()).add(" \n");
+		joiner.add("VM vendor ").add(runtimeBean.getVmVendor()).add(" \n");;
+		joiner.add("VM version ").add(runtimeBean.getVmVersion()).add(" \n");
+		joiner.add("Start time of VM ").add(String.valueOf(runtimeBean.getStartTime())).add(" \n");;
+		joiner.add("Uptime of VM ").add(String.valueOf(runtimeBean.getUptime())).add(" \n");;
+		
+		return joiner.toString();
+		
+	}
+	
+
 
 }
